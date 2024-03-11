822c87dbfd65590e924a7091f2ceb1d8b9720969
Allan Lima <allanpatrickdesiqueiralima@gmail.com>
2024-03-11 20:33:31 -0300

    Icons with update const
    update with icons menu and close menu

--- Nik-nails/src/components/navbar.jsx
+++ Nik-nails/src/components/navbar.jsx
@@ -1,37 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './navbar.css';
-import {AiFillContacts} from 'react-icons/ai';
+import {AiFillContacts, AiOutlineMenuFold, AiOutlineClose} from 'react-icons/ai';
 //
 const Navbar = () => {
+    const [nav, setNav] = useState(false);
+    const handleNav = () => {
+        setNav(!nav);
+    }
+
     return (
-
-        <div className='text-white
-         flex justify-evenly font-bold items-center h-24 max-w-[1240px] mx-auto
-                  '>
-                
-
-                <ul className='flex justify-around'>
-                    <li className='p-4'>Products</li>
+        <div className>
+            <head className='text-white
+            flex justify-evenly font-bold items-center h-24 max-w-[1242px] mx-auto
+            '>
+                <ul className='flex hidden justify-around'>
+                    <li className='p-4'>Catalog</li>
                     <li className='p-4'>Scheduling</li>
                     <li className='p-4'>About</li>
+                  
                 </ul>
-                <div>
-                    <h1 className= 'text-4xl p-6 py-4 px-8 text-pink-400'>Nik Nails</h1>
+
+                {/* useState to close the menu*/}
+                <div onClick={handleNav} className='transform rotate-180 text-sky-300' >
+                    {!nav ? <AiOutlineClose size={24}/> : <AiOutlineMenuFold size={24} />}
+                </div>
+
+                <div className={!nav ? 'fixed text-white text-left h-full border-r border-zinc-600 top-0 left-0' : 'fixed hidden'}>
+                    <ul className='pt-20 pl-0.1' >
+                        <li className='p-4 border-b border-zinc-600'>Catalog</li>
+                        <li className='p-4 border-b border-zinc-600'>Scheduling</li>
+                        <li className='p-4 border-b border-zinc-600'>About</li>
+                    </ul>
                 </div>
 
                 <div>
-                    <button className='flex  items-center ml-2 bg-pink-300
-                    hover:bg-pink-400 pr-5 pl-5 rounded-lg
-                    '
+                    <h1 className= 'text-3xl p-6 py-4 px-8 text-sky-300'>Nik Nails</h1>
+                </div>
+
+                <div>
+                    <button className='flex g-1 items-center ml-2 bg-zinc-100 text-sky-300
+                        hover:bg-sky-400 pr-2 pl-2 rounded-lg p-1 
+                        '
                     >
-                         <AiFillContacts size={32}/>Contact
+                        <AiFillContacts size={30}/>
                     </button>
                 </div> 
-                <div>
-                   
-                </div>
+            </head>
             <div>
-                
+                <main>
+
+                </main>
+            </div>
+            <div>
+                <footer>
+
+                </footer>
             </div>
         </div>
     )
